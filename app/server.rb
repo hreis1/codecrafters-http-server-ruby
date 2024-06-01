74d4e457a6a7f759162517c63c7034752b478277
hreis <ph.reis@discente.ufma.br>
2024-06-01 00:30:12 -0300

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -7,28 +7,39 @@ loop do
   request = client_socket.gets
   verb, path, protocol = request.split(" ")
   headers = {}
-  while line = client_socket.gets.split(' ', 2)
+  while line = client_socket.gets.split(" ", 2)
     break if line[0] == ""
-    headers[line[0].chop] = line[1].strip
+    headers[line[0].chop.downcase!] = line[1].strip
   end
   case path
   when "/"
     client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
   when /\/echo\/.*/
     content = path.split("/").last
-    if headers['Accept-Encoding'] == 'gzip'
+    invalid_headers = []
+    gzip = false
+    headers["accept-encoding"]&.split(', ')&.each do |header|
+      if header != "gzip"
+        invalid_headers << header
+      else
+        gzip = true
+      end
+    end
+    if invalid_headers.any? && gzip
       client_socket.puts "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length:#{content.length}\r\n\r\n#{content}"
-    else
+    elsif invalid_headers.any?
       client_socket.puts "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:#{content.length}\r\n\r\n#{content}"
+    else
+      client_socket.puts "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length:#{content.length}\r\n\r\n#{content}"
     end
   when /\/user-agent/
-    agent = headers['User-Agent']
+    agent = headers["user-agent"]
     client_socket.puts "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{agent.length}\r\n\r\n#{agent}"
   when /\/files/
     filename = path.split("/").last
     directory = ARGV[1]
     file_path = directory+filename
-    if verb == 'GET'
+    if verb == "GET"
       begin
         file = File.open("#{file_path}", "r").read
         client_socket.puts "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: #{file.length}\r\n\r\n#{file}"
@@ -36,7 +47,7 @@ loop do
         client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
       end
     else
-      content = client_socket.read(headers['Content-Length'].to_i)
+      content = client_socket.read(headers["content-length"].to_i)
       begin
         File.write(file_path, content)
         client_socket.puts "HTTP/1.1 201 Created\r\n\r\n"
